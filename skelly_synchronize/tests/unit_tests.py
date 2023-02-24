bb068005f236a7005bcd1a7c9641c43fc3323201
philipqueen <queen.philip@gmail.com>
2023-02-24 08:55:25 -0700

    change list to dict in unit tests

--- skelly_synchronize/tests/unit_tests.py
+++ skelly_synchronize/tests/unit_tests.py
@@ -6,17 +6,20 @@ from skelly_synchronize.skelly_synchronize import VideoSynchronize
 
 @pytest.fixture
 def lag_list():
-    return [0.0, 4.3402267573696145, 9.475963718820863]
+    return {"Cam1": 0.0, "Cam2": 4.3402267573696145, "Cam3": 9.475963718820863}
 
 
 def test_normalize_lag_list(lag_list):
-    """Actually need to split normalize lag list into different function"""
     test_synchronize = VideoSynchronize(
         "iPhoneTesting",
         Path("/Users/philipqueen/Documents/Humon Research Lab/FreeMocap_Data"),
     )
-    assert test_synchronize._normalize_lag_dictionary(lag_list) == [
-        9.475963718820863,
-        5.135736961451248,
-        0.0,
-    ], "Lag dict did not normalize correctly"
+    assert test_synchronize._normalize_lag_dictionary(lag_list) == {
+        "Cam1": 9.475963718820863,
+        "Cam2": 5.135736961451248,
+        "Cam3": 0.0,
+    }, "Lag dict did not normalize correctly"
+
+if __name__ == "__main__":
+    test_normalize_lag_list(lag_list)
+    print("tests passed")
