5d35dabae744787835f09f86a95d5a02c895d411
philipqueen <queen.philip@gmail.com>
2023-06-05 14:01:23 -0600

    change figshare path again, along with file name

--- skelly_synchronize/system/paths_and_file_names.py
+++ skelly_synchronize/system/paths_and_file_names.py
@@ -1,5 +1,6 @@
 # directory names
 SYNCHRONIZED_VIDEOS_FOLDER_NAME = "synchronized_videos"
+RAW_VIDEOS_FOLDER_NAME = "raw_videos"
 AUDIO_FILES_FOLDER_NAME = "audio_files"
 TRIMMED_AUDIO_FOLDER_NAME = "trimmed_audio"
 
@@ -14,5 +15,5 @@ AUDIO_NAME = "Audio_information"
 LAG_DICTIONARY_NAME = "Lag_dictionary"
 
 # figshare info
-FIGSHARE_ZIP_FILE_URL = "https://figshare.com/ndownloader/files/41066321"
-FIGSHARE_SAMPLE_DATA_FILE_NAME = "freemocap_sample_data"
+FIGSHARE_ZIP_FILE_URL = "https://figshare.com/ndownloader/files/41066489"
+FIGSHARE_SAMPLE_DATA_FILE_NAME = "skelly_synchronize_sample_data"
