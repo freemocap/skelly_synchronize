691c4c88a3dc47978659f8517b2de9c628f97954
philipqueen <queen.philip@gmail.com>
2023-06-15 11:59:04 -0600

    add transposition dictionary

---
+++ skelly_synchronize/utils/ffmpeg_transposition_dictionary.py
@@ -0,0 +1,7 @@
+tranposition_dictionary = {
+            90.0: 'transpose=cclock',
+            -270.0: 'transpose=cclock',
+            -90.0: 'transpose=clock',
+            270.0: 'transpose=clock',
+            180.0: 'transpose=cclock,transpose=cclock',
+        }
