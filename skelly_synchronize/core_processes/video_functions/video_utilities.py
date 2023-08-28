c640531ff4880cddf5679195736134bd0b4b4144
philipqueen <queen.philip@gmail.com>
2023-08-28 10:28:52 -0700

    only split at first underscore

--- skelly_synchronize/core_processes/video_functions/video_utilities.py
+++ skelly_synchronize/core_processes/video_functions/video_utilities.py
@@ -144,7 +144,7 @@ def attach_audio_to_videos(
     ) as temp_dir:
         for video in get_video_file_list(synchronized_video_folder_path):
             video_name = video.stem
-            audio_filename = f"{str(video_name).split('_')[-1]}.wav"
+            audio_filename = f"{str(video_name).split('_', maxsplit=1)[-1]}.wav"
             output_video_pathstring = str(
                 Path(temp_dir) / f"{video_name}_with_audio_temp.mp4"
             )
