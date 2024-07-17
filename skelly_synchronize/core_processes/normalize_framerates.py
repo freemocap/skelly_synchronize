794201d1b0121a3478e787f8122e176589fc40fe
philipqueen <queen.philip@gmail.com>
2024-07-17 17:52:52 -0600

    linting and type checking

--- skelly_synchronize/core_processes/normalize_framerates.py
+++ skelly_synchronize/core_processes/normalize_framerates.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 from skelly_synchronize.core_processes.video_functions.ffmpeg_functions import (
     normalize_framerates_in_video_ffmpeg,
 )
@@ -7,12 +7,14 @@ from skelly_synchronize.system.paths_and_file_names import NORMALIZED_VIDEOS_FOL
 
 from skelly_synchronize.utils.path_handling_utilities import create_directory
 
+standard_audio_sample_rate = 44100
+
 
 def normalize_framerates(
     raw_video_folder_path: Path,
     video_info_dict: Dict[str, dict],
     fps_list: List[float],
-    audio_samplerate_list: List[float] = None,
+    audio_samplerate_list: Optional[List[float]] = None,
 ) -> Path:
     """Normalize the frame rates of a list of videos. Also normalize audio sample rates, if given"""
     normalized_videos_folder_path = create_directory(
@@ -23,11 +25,11 @@ def normalize_framerates(
     fps_list.sort()
     desired_fps = min(fps_list)
 
-    if audio_samplerate_list is not None:
+    if audio_samplerate_list:
         audio_samplerate_list.sort()
         desired_audio_sample_rate = min(audio_samplerate_list)
     else:
-        desired_audio_sample_rate = 44100
+        desired_audio_sample_rate = standard_audio_sample_rate
 
     for video_dict in video_info_dict.values():
         normalize_framerates_in_video_ffmpeg(
@@ -36,7 +38,7 @@ def normalize_framerates(
                 normalized_videos_folder_path / f"{video_dict['camera name']}.mp4"
             ),
             desired_fps=desired_fps,
-            desired_sample_rate=desired_audio_sample_rate,
+            desired_sample_rate=int(desired_audio_sample_rate),
         )
 
     return normalized_videos_folder_path
